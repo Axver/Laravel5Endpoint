d2fd074eab76a176839de77e63063ab86902b5b3
Axver <ecimoramora@gmail.com>
2020-05-16 20:24:23 +0700

    Implementasi Redis dan Expired Token

--- app/Http/Controllers/ForgotController.php
+++ app/Http/Controllers/ForgotController.php
@@ -6,6 +6,11 @@ use App\Mail\ForgotEmail;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Cache;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
+use JWTAuth;
+use JWTAuthException;
 
 class ForgotController extends Controller
 {
@@ -18,6 +23,11 @@ class ForgotController extends Controller
                 'email'=>'required',
             ]);
         $email=$request->input('email');
+        $credential=
+            [
+                'email'=>$email
+
+            ];
         $CheckProfile = User::where('email', '=', $email)->first();
         if(!$CheckProfile)
         {
@@ -28,11 +38,47 @@ class ForgotController extends Controller
             );
         }
 
+        if (Cache::has('token')) {
+//            Masih belum 3 menit
+            $value = Cache::get('key');
+            $token = Cache::get('token');
+            Mail::to($email)->send(new ForgotEmail($value,$email));
+
+            $response=[
+                'status'=>'success',
+                'message'=>'Resend Verification Code',
+                'token'=>$token
+
+            ];
+        }
+        else
+        {
+//            Delete all token data
+            DB::table('password_resets')->where('email', $email)->delete();
 //        send email to user
-        $code=rand(1000,9999);
-        Mail::to($email)->send(new ForgotEmail($code,$email));
+            $code=rand(100000,999999);
+            Mail::to($email)->send(new ForgotEmail($code,$email));
 //        Simpan kode kedalam database
-        return $code;
+            DB::table('password_resets')->insert(
+                ['email' => $email, 'token' => $code]
+            );
+
+            $token=md5(uniqid(rand(), true));
+
+            $expiresAt = Carbon::now()->addMinutes(3);
+            Cache::put('key', $code, $expiresAt);
+            Cache::put('token', $token, $expiresAt);
+            $value = Cache::get('key');
+            $response=[
+                'status'=>'success',
+                'message'=>'Adding New Verification Code',
+                'token'=>$token
+            ];
+
+        }
+
+
+        return $response;
 
     }
 
