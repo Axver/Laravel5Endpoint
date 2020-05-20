9699d3eb7dd908a2f75c661a9c672b92246a725b
Axver <ecimoramora@gmail.com>
2020-05-20 15:46:19 +0700

    PDF js

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -2,69 +2,150 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=.5, maximum-scale=12.0, minimum-scale=.25, user-scalable=yes"/>
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="{!! asset('build/pdf.js') !!}"></script>
-    <script src="{!! asset('build/pdf.worker.js') !!}"></script>
+{{--    <script src="{!! asset('build/pdf.js') !!}"></script>--}}
+{{--    <script src="{!! asset('build/pdf.worker.js') !!}"></script>--}}
+    <script src="https://unpkg.com/pdfjs-dist@2.0.489/build/pdf.min.js"></script>
     <title>PDF Viewer</title>
 </head>
 <body>
 
 <input type="hidden" id="nama" value="<?php echo $nama; ?>">
-<div align="center">
-    <br/>
-    <canvas id="the-canvas"></canvas>
+<body>
+<div id="app">
+    <div role="toolbar" id="toolbar">
+        <div id="pager">
+            <button data-pager="prev">prev</button>
+            <button data-pager="next">next</button>
+
+        </div>
+        <div id="page-mode">
+            <label>Page Mode <input type="number" value="1" min="1"/></label>
+        </div>
+    </div>
+    <div id="viewport-container"><div role="main" id="viewport"></div></div>
 </div>
 
+
 <script>
+    let x=1;
 
-        let nama=document.getElementById('nama').value;
+    (function() {
+        let currentPageIndex = 0;
+        let pageMode = 1;
+        let cursorIndex = Math.floor(currentPageIndex / pageMode);
+        let pdfInstance = null;
+        let totalPagesCount = 0;
+        let pdfViewport;
 
-        // If absolute URL from the remote server is provided, configure the CORS
-        // header on that server.
-        var url = nama;
-
-        // Loaded via <script> tag, create shortcut to access PDF.js exports.
-        var pdfjsLib = window['pdfjs-dist/build/pdf'];
-
-        // The workerSrc property shall be specified.
-        pdfjsLib.GlobalWorkerOptions.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.js';
-
-        // Asynchronous download of PDF
-        var loadingTask = pdfjsLib.getDocument(url);
-        loadingTask.promise.then(function(pdf) {
-            console.log('PDF loaded');
-
-            // Fetch the first page
-            var pageNumber = 1;
-            pdf.getPage(pageNumber).then(function(page) {
-                console.log('Page loaded');
-
-                var scale = 1.5;
-                var viewport = page.getViewport({scale: scale});
-
-                // Prepare canvas using PDF page dimensions
-                var canvas = document.getElementById('the-canvas');
-                var context = canvas.getContext('2d');
-                canvas.height = viewport.height;
-                canvas.width = viewport.width;
-
-                // Render PDF page into canvas context
-                var renderContext = {
-                    canvasContext: context,
-                    viewport: viewport
-                };
-                var renderTask = page.render(renderContext);
-                renderTask.promise.then(function () {
-                    console.log('Page rendered');
-                });
+        const viewport = document.querySelector("#viewport");
+        window.initPDFViewer = function(pdfURL) {
+            pdfjsLib.getDocument(pdfURL).then(pdf => {
+                pdfInstance = pdf;
+                totalPagesCount = pdf.numPages;
+                initPager();
+                initPageMode();
+                render();
             });
-        }, function (reason) {
-            // PDF loading error
-            console.error(reason);
-        });
+        };
+
+        function onPagerButtonsClick(event) {
+            const action = event.target.getAttribute("data-pager");
+            if (action === "prev") {
+                if (currentPageIndex === 0) {
+                    return;
+                }
+                currentPageIndex -= pageMode;
+                if (currentPageIndex < 0) {
+                    currentPageIndex = 0;
+                }
+                render();
+            }
+            if (action === "next") {
+                if (currentPageIndex === totalPagesCount - 1) {
+                    return;
+                }
+                currentPageIndex += pageMode;
+                if (currentPageIndex > totalPagesCount - 1) {
+                    currentPageIndex = totalPagesCount - 1;
+                }
+                render();
+            }
+        }
+        function initPager() {
+            const pager = document.querySelector("#pager");
+            pager.addEventListener("click", onPagerButtonsClick);
+            return () => {
+                pager.removeEventListener("click", onPagerButtonsClick);
+            };
+        }
+
+        function onPageModeChange(event) {
+            pageMode = Number(event.target.value);
+            render();
+        }
+        function initPageMode() {
+            const input = document.querySelector("#page-mode input");
+            input.setAttribute("max", totalPagesCount);
+            input.addEventListener("change", onPageModeChange);
+            return () => {
+                input.removeEventListener("change", onPageModeChange);
+            };
+        }
+
+        function render() {
+            cursorIndex = Math.floor(currentPageIndex / pageMode);
+            const startPageIndex = cursorIndex * pageMode;
+            const endPageIndex =
+                startPageIndex + pageMode < totalPagesCount
+                    ? startPageIndex + pageMode - 1
+                    : totalPagesCount - 1;
+
+            const renderPagesPromises = [];
+            for (let i = startPageIndex; i <= endPageIndex; i++) {
+                renderPagesPromises.push(pdfInstance.getPage(i + 1));
+            }
+
+            Promise.all(renderPagesPromises).then(pages => {
+                const pagesHTML = `<div style="width: ${
+                    pageMode > 1 ? "50%" : "100%"
+                }"><canvas></canvas></div>`.repeat(pages.length);
+                viewport.innerHTML = pagesHTML;
+                pages.forEach(renderPage);
+            });
+        }
+
+        function renderPage(page) {
+            pdfViewport = page.getViewport(1);
+
+            const container =
+                viewport.children[page.pageIndex - cursorIndex * pageMode];
+            pdfViewport = page.getViewport(container.offsetWidth / pdfViewport.width);
+            const canvas = container.children[0];
+            const context = canvas.getContext("2d");
+            canvas.height = pdfViewport.height;
+            canvas.width = pdfViewport.width;
+
+            page.render({
+                canvasContext: context,
+                viewport: pdfViewport
+            });
+
+        }
+
+
+    })();
+
+    let name=document.getElementById('nama').value;
+    initPDFViewer(name);
+
+
 
 </script>
+
+
 </body>
 </html>
